80b42fe949f8505642b622fc66f459a47e4ba114
Lauri Kainulainen <lauri.kainulainen@gmail.com>
2018-10-05 23:30:15 +0300

    ansible update for techtest server

--- ansible/files/update-techtest_journey_python.sh
+++ ansible/files/update-techtest_journey_python.sh
@@ -6,22 +6,31 @@ cd $WD
 
 # pull all the branches
 for b in `git branch -r | grep -v -- '->'`; do
+    echo "getting branch $b";
     git branch --track ${b##origin/} $b;
+    echo "pulling branch $b";
+    git checkout ${b##origin/};
+    git pull;
 done
+
 # take the branch with the latest commit
 LATEST_BRANCH=$(git branch --sort=committerdate --format="%(refname:short)" | tail -n1)
+echo "branch with latest commit $LATEST_BRANCH"
 git checkout $LATEST_BRANCH
-git pull
+git reset --hard origin/$LATEST_BRANCH
 
 # direct installation
+echo "Installing dependencies"
 pip3 install -r requirements.txt
 systemctl restart techtest_journey_python
 echo "Build barebones done"
 
 # docker installation
-docker build . --tag techtest_journey_python
+docker rmi --force techtest_journey_python
 docker kill ttjp
-docker rm ttjp
-docker run -p 7899:8000 --name ttjp techtest_journey_python
+docker rm --force ttjp
+docker build . --tag techtest_journey_python
+docker run -d -p 9876:7890 --name ttjp -v /root/dockerized_planets.json:/planets.json \
+    -e DATA_FILE=/planets.json techtest_journey_python
 
 echo "Build all done"
